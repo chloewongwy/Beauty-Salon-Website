48e5b0cd89a57082d53ea2d8f3ae8d352bfd982b
Wong Wing Yi, Chloe <iamwongwingyichloe@gmail.com>
2025-08-19 16:40:05 +0800

    upload code.

---
+++ web-src/layout.js
@@ -0,0 +1,13 @@
+// JavaScript Documentfunction 
+
+// Navigation Bar
+function myFunction() {
+	var x = document.getElementById("myHeader");
+	if (x.className === "header") {
+		x.className += " responsive";
+	} else {
+		x.className = "header";
+	}
+}
+
+	
---
+++ web-src/serviceScrollBrand.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+// scroll brand horizontally 
+
+function scrolll() {
+	var left = document.querySelector(".scroll-images");
+    left.scrollBy(-350, 0)
+}
+
+function scrollr() {
+    var right = document.querySelector(".scroll-images");
+    right.scrollBy(350, 0)
+}
